747e67b9ddcfe8687265ba1abae22a0b6665d594
youngsookim <kim206gh@naver.com>
2021-06-24 17:31:37 +0900

    Timer 예제 완성

--- aop/src/main/java/com/example/aop/controller/RestApiController.java
+++ aop/src/main/java/com/example/aop/controller/RestApiController.java
@@ -1,7 +1,9 @@
 package com.example.aop.controller;
 
+import com.example.aop.annotation.Timer;
 import com.example.aop.dto.User;
 import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -24,4 +26,27 @@ public class RestApiController {
         return user;
     }
 
+    @Timer
+    @DeleteMapping("/delete")
+    public void delete() throws InterruptedException {
+        // need db logic
+
+
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Thread.sleep(1000 * 2);
+
+        stopWatch.stop();
+        System.out.println("total time : " + stopWatch.getTotalTimeSeconds());
+
+    }
+
+    @PostMapping("/put")
+    public User put(@RequestBody User user) {
+        System.out.println("put");
+        System.out.println(user);
+
+        return user;
+    }
 }
