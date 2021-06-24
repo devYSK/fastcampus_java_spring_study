1247ccfa026855a0ef5a22d71905ac94c95e9e71
youngsookim <kim206gh@naver.com>
2021-06-24 23:45:32 +0900

    object mapper 실습

---
+++ object-mapper/src/main/java/dto/Car.java
@@ -0,0 +1,32 @@
+package dto;
+
+public class Car {
+
+    private String name;
+    private String carNumber;
+    private String type;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
