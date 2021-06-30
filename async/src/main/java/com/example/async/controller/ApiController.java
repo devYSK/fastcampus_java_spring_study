c9011755b52c705e239ebf8a237723937c4f8366
youngsookim <kim206gh@naver.com>
2021-07-01 01:49:51 +0900

    비동기 처리 예제 공부 완료

--- async/src/main/java/com/example/async/controller/ApiController.java
+++ async/src/main/java/com/example/async/controller/ApiController.java
@@ -1,13 +1,30 @@
 package com.example.async.controller;
 
+import com.example.async.service.AsyncService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    public String hello() {
+
+    private final AsyncService asyncService;
+
+
+    @GetMapping("/hello")
+    public CompletableFuture hello() throws InterruptedException {
+        asyncService.hello();
+
+        log.info("method end");
+        return asyncService.run();
 
     }
 }
