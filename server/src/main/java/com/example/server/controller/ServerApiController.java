3d239fc0326af3a0e224e5bb28e86e27a157238a
youngsookim <kim206gh@naver.com>
2021-07-07 21:53:44 +0900

    네이버 open Api 연동 학습

--- server/src/main/java/com/example/server/controller/ServerApiController.java
+++ server/src/main/java/com/example/server/controller/ServerApiController.java
@@ -3,7 +3,16 @@ package com.example.server.controller;
 import com.example.server.dto.Req;
 import com.example.server.dto.User;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.buf.Utf8Encoder;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @Slf4j
 @RestController
@@ -18,6 +27,41 @@ public class ServerApiController {
         user.setAge(age);
 
         return user;
+    }
+
+    //https://openapi.naver.com/v1/search/local.json
+    // ?query=%EC%A3%BC%EC%8B%9D
+    // &display=10
+    // &start=1
+    // &sort=random
+    @GetMapping("/naver")
+    public String naver() {
+
+        String query = "갈비집";
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/local.json")
+                .queryParam("query", query)
+                .queryParam("display", 10)
+                .queryParam("start", 1)
+                .queryParam("sort", "random")
+                .encode()
+                .build()
+                .toUri();
+
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        RequestEntity<Void> req = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", "uDUTuy6vUHuCPVUnf3Vj")
+                .header("X-Naver-Client-Secret", "0VCQVBLBA0")
+                .build();
+
+
+        ResponseEntity<String> result = restTemplate.exchange(req, String.class);
+
+        return result.getBody();
     }
 
     @PostMapping("/user/{userId}/name/{userName}")
