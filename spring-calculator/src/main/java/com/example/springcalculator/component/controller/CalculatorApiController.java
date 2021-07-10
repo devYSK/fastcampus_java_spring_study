8aa21b682fe3ebbcbd48c5c767b0d1a6857cc1e8
youngsookim <kim206gh@naver.com>
2021-07-11 00:01:29 +0900

    CalculatorApiController
    minus 메서드 Get -> PostMapping으로 변경

--- spring-calculator/src/main/java/com/example/springcalculator/component/controller/CalculatorApiController.java
+++ spring-calculator/src/main/java/com/example/springcalculator/component/controller/CalculatorApiController.java
@@ -2,10 +2,7 @@ package com.example.springcalculator.component.controller;
 
 import com.example.springcalculator.component.Calculator;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +18,7 @@ public class CalculatorApiController {
         return calculator.sum(x, y);
     }
 
-    @GetMapping("/minus")
+    @PostMapping("/minus")
     public int minus(@RequestParam int x, @RequestParam int y) {
         return calculator.minus(x, y);
     }
