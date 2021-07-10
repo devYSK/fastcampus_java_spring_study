06c95db8e108f81b5a0b082a2821f261d9e43c6b
youngsookim <kim206gh@naver.com>
2021-07-10 23:44:17 +0900

    Calculator  단위테스트

--- spring-calculator/src/test/java/com/example/springcalculator/component/DollarCalculatorTest.java
+++ spring-calculator/src/test/java/com/example/springcalculator/component/DollarCalculatorTest.java
@@ -1,11 +1,34 @@
 package com.example.springcalculator.component;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 
-@Import(MarketApi.class)
+import static org.mockito.Mockito.when;
+
+@Import({MarketApi.class, DollarCalculator.class})
+@SpringBootTest
 public class DollarCalculatorTest {
 
-    @MockBean // 스프링에서는 빈으로 관리하기 떄문 
+    @MockBean // 스프링에서는 빈으로 관리하기 떄문
     private MarketApi marketApi;
+
+    @Autowired
+    private Calculator calculator;
+
+
+    @Test
+    public void dollarCalculatorTest() {
+        when(marketApi.connect()).thenReturn(3000);
+
+        int sum = calculator.sum(10, 10);
+        int minus = calculator.minus(10, 10);
+        Assertions.assertEquals(60000, sum);
+        Assertions.assertEquals(0, minus);
+    }
 }
+
+
